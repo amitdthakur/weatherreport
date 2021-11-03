90ffd1260e21e1daa33b0af9a05f78a0a66cf86d
Amit <thakuramit@gmail.com>
2021-11-03 23:12:34 +0530

    Database scripts kept in single line

--- sql/dataBaseScripts.sql
+++ sql/dataBaseScripts.sql
@@ -1,19 +1,7 @@
 #Table creation queries:
-CREATE TABLE sensorMetrics (
-    sensorId int,
-    temperature float,
-    humidity int,
-    windSpeed float,
-    pressure float,
-    lastUpdated TIMESTAMP
-);
+CREATE TABLE sensorMetaData (sensorId int,countryName varchar(255),cityName varchar(255),created TIMESTAMP);
 
-CREATE TABLE sensorMetaData (
-    sensorId int,
-    countryName varchar(255),
-    cityName varchar(255),
-    created TIMESTAMP
-);
+CREATE TABLE sensorMetrics (sensorId int, temperature float,humidity int,windSpeed float,pressure float,lastUpdated TIMESTAMP);
 
 #Primary key:
 ALTER TABLE sensorMetaData ADD PRIMARY KEY(sensorId);
