d5b233d0ad2ea0f16ff4e9ddf311b349c7e01d7e
Amit <thakuramit@gmail.com>
2021-11-03 12:05:18 +0530

    Removed unwanted lombok annotations

--- src/main/java/com/weatherreport/sensors/exception/DateOlderThan30DaysException.java
+++ src/main/java/com/weatherreport/sensors/exception/DateOlderThan30DaysException.java
@@ -2,11 +2,12 @@ package com.weatherreport.sensors.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+
 @AllArgsConstructor
+@Getter
 public class DateOlderThan30DaysException extends Exception {
 
   private String message;
--- src/main/java/com/weatherreport/sensors/exception/SensorAlreadyExistsException.java
+++ src/main/java/com/weatherreport/sensors/exception/SensorAlreadyExistsException.java
@@ -2,11 +2,11 @@ package com.weatherreport.sensors.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class SensorAlreadyExistsException extends Exception {
 
   private String message;
